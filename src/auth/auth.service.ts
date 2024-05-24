7661a9a497390c64fb2d4eb1b33fa3437fd23fb7
niko <nikolay17538583@gmail.com>
2024-05-24 22:48:04 +0300

    refreshToken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Injectable,
   NotFoundException,
   UnauthorizedException,
@@ -7,25 +8,47 @@ import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 import { AuthDto } from './dto/auth.dto';
 import { verify } from 'argon2';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
+  EXPIRE_DAY_REFRESH_TOKEN = 1;
+  REFRESH_TOKEN_NAME = 'refreshToken';
+
   constructor(
     private jwt: JwtService,
     private userService: UserService,
   ) {}
 
   async login(dto: AuthDto) {
-    return dto;
+    const { password, ...user } = await this.validateUser(dto);
+    const tokens = this.issueTokens(user.id);
+
+    return {
+      user,
+      ...tokens,
+    };
+  }
+
+  async register(dto: AuthDto) {
+    const oldUser = await this.userService.getByEmail(dto.email);
+    if (oldUser) throw new BadRequestException('User already exists');
+
+    const { password, ...user } = await this.userService.create(dto);
+
+    const tokens = this.issueTokens(user.id);
+
+    return {
+      user,
+      ...tokens,
+    };
   }
 
   private issueTokens(userId: string) {
     const data = { id: userId };
-
     const accessToken = this.jwt.sign(data, {
       expiresIn: '1h',
     });
-
     const refreshToken = this.jwt.sign(data, {
       expiresIn: '7d',
     });
@@ -35,13 +58,37 @@ export class AuthService {
 
   private async validateUser(dto: AuthDto) {
     const user = await this.userService.getByEmail(dto.email);
-
     if (!user) throw new NotFoundException('User not found');
 
     const isValid = await verify(user.password, dto.password);
-
     if (!isValid) throw new UnauthorizedException('Invalid password');
 
     return user;
+  }
+
+  addRefreshTokenToResponce(res: Response, refreshToken: string) {
+    const expiresIn = new Date();
+    expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN);
+
+    res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+      httpOnly: true,
+      domain: 'localhost',
+      expires: expiresIn,
+      secure: true,
+      sameSite: 'none', // lax if prod
+    });
+  }
+
+  removeRefreshTokenToResponce(res: Response, refreshToken: string) {
+    const expiresIn = new Date();
+    expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN);
+
+    res.cookie(this.REFRESH_TOKEN_NAME, '', {
+      httpOnly: true,
+      domain: 'localhost',
+      expires: new Date(0),
+      secure: true,
+      sameSite: 'none', // lax if prod
+    });
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { hash } from 'argon2';
+import { AuthDto } from 'src/auth/dto/auth.dto';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
@@ -24,6 +26,18 @@ export class UserService {
       include: {
         tasks: true,
       },
+    });
+  }
+
+  async create(dto: AuthDto) {
+    const user = {
+      email: dto.email,
+      name: '',
+      password: await hash(dto.password),
+    };
+
+    return this.prisma.user.create({
+      data: user,
     });
   }
 }
